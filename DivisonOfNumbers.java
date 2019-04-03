2e90157a073e816d299a668b3fa035e56edfbbb2
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-03 20:03:31 +0530

    Add files via upload
    Divison of Numbers

---
+++ DivisonOfNumbers.java
@@ -0,0 +1,27 @@
+/* Program of Divison of Numbers*/
+
+
+import java.util.*;
+public class DivisonOfNumbers {
+
+	public static void main(String[] args) {
+		
+	Scanner scan=new Scanner(System.in);
+	
+	System.out.println("I am saloni gupta......");
+	System.out.println("i m doing the divison of two values");
+	
+	System.out.println("enter the number 1");
+	
+	float num1=scan.nextInt();
+	
+	System.out.println("enter the number 2");
+	
+	float num2=scan.nextInt();
+	
+	float temp=num1/num2;
+	
+	System.out.println("the result is...."+temp);
+	}
+
+}

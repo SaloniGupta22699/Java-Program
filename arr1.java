7314a80f50eb7e7293e6092ae47ffffdd97fb73e
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-03 22:54:12 +0530

    Add files via upload

---
+++ arr1.java
@@ -0,0 +1,18 @@
+/* Program of Printing the element of an array*/
+
+
+class arr1 {
+	
+	public static void main(String args[]){
+
+	int arr[]={2,6,8,12,10};
+	
+	
+	for(int i=0;i<5;i++)
+	{
+		System.out.println(arr[i]);
+	}
+	
+	
+}
+}

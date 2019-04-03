3279a51d430b4b8d09694f762cdb43a99bf8cc55
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-03 22:46:39 +0530

    Add files via upload

---
+++ Hcf.java
@@ -0,0 +1,18 @@
+/* Program of HCF of Two Numbers*/
+
+class Hcf
+{
+	public static void main(String arg[])	
+	{
+   	int n1=Integer.parseInt(arg[0]);
+   	int n2=Integer.parseInt(arg[1]); 
+	int temp;
+   	while (n2 > 0)
+   	{
+   	     temp = n2;
+   	     n2 = n1% n2; 
+   	     n1 = temp;
+   	}
+	System.out.println("hcf="+n1);
+	}	
+}

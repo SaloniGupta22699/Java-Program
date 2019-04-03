f578fc24c3e4e9a0056cc6ee5020f81e84882f46
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-03 20:02:51 +0530

    Add files via upload
    Multiply Two Number

---
+++ MultiplyTwoNumber.java
@@ -0,0 +1,28 @@
+/* Program of Multiplying Two Numbers */
+import java.util.*;
+public class MultiplyTwoNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner scan=new Scanner(System.in);
+		
+		
+		System.out.println("enter the number 1");
+		
+		int num1=scan.nextInt();
+		
+		
+		System.out.println("enter the number 2");
+		
+		int num2=scan.nextInt();
+		
+		int temp=num1*num2;
+		
+		System.out.println("the result is========"+temp);
+		
+
+		
+		
+	}
+
+}

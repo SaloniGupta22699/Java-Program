c9189d9fc1408ea1dfb5f987b3fd310dabde2367
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-11 09:08:48 +0530

    Add files via upload

---
+++ FindncRnPr.java
@@ -0,0 +1,29 @@
+/* Java Program Example - Find ncR nPr */
+		
+import java.util.Scanner;
+
+public class FindncRnPr
+{
+    public static int fact(int num)
+    {
+        int fact=1, i;
+        for(i=1; i<=num; i++)
+        {
+            fact = fact*i;
+        }
+        return fact;
+    }
+    public static void main(String args[])
+    {
+        int n, r;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter Value of n : ");
+        n = scan.nextInt();
+        System.out.print("Enter Value of r : ");
+        r = scan.nextInt();
+		
+        System.out.print("NCR = " +(fact(n)/(fact(n-r)*fact(r))));
+        System.out.print("\nNPR = " +(fact(n)/(fact(n-r))));
+    }
+}

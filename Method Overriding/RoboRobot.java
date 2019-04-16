7d98f2e3f6da4a7ab71e3ac51f8937b8cce0a1be
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-16 20:48:26 +0530

    Add files via upload

---
+++ Method Overriding/RoboRobot.java
@@ -0,0 +1,19 @@
+
+public class RoboRobot extends Robot{
+	
+	public RoboRobot(){
+		System.out.println("RoboRobot constructor is called...");
+	}
+
+	@Override
+	public void speek() {
+		System.out.println("_@_@_@_How I can assist you sir!");
+	}
+
+	@Override
+	public void dance() {
+		System.out.println("_@)@Robot is dancing......");
+		
+	}
+
+}

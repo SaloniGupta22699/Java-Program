5532a9814f26cefc2bc3950cdcff6239c78da8cd
SaloniGupta22699 <38486816+SaloniGupta22699@users.noreply.github.com>
2019-04-16 20:49:34 +0530

    Add files via upload

---
+++ Method Overriding/RoboRobotMain.java
@@ -0,0 +1,12 @@
+
+public class RoboRobotMain {
+
+	public static void main(String[] args) {
+		RoboRobot roboRobot=new RoboRobot();
+		roboRobot.jump();
+		roboRobot.walk();
+		roboRobot.speek();
+
+	}
+
+}
